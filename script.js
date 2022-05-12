29b2ff1dd9122306b04038663d12370d877047b7
Sani Letchu <sani.letchu@gmail.com>
2022-05-12 11:09:56 +0300

    Working function that shapes the grid according to given number

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameContainer = document.querySelector('#gamecontainer');
+
+
+function createDivs(size) {
+    gameContainer.style.cssText = `grid-template-columns: repeat(${size}, minmax(0, 1fr)); grid-template-rows: repeat(${size}, minmax(0, 1fr));`;
+    for(let i = 0; i < size; i++) {
+        for(let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.classList.add('gamecube')
+            gameContainer.appendChild(div);
+        }
+    }
+    
+}
+
+createDivs(3);
