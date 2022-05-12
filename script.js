114953d850519572ea8e2dcf397cd7bed1f55f1e
Sani Letchu <sani.letchu@gmail.com>
2022-05-12 12:13:59 +0300

    Two new buttons that change the color of the pen used

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const gameContainer = document.querySelector('#gamecontainer');
 
+// 0 = grey, 1 = Random color
+let colorChoice = 0;
+
+function returnBackgroundColor() {
+    if(colorChoice == 0) {
+        return "background-color: grey";
+    }
+    else {
+        // Credits: https://stackoverflow.com/questions/1484506/random-color-generator
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return `background-color: ${color}`;
+    }
+}
 
 function createDivs(size) {
     while(gameContainer.firstChild) {
@@ -12,7 +29,7 @@ function createDivs(size) {
             const div = document.createElement('div');
             div.classList.add('gamecube');
             div.addEventListener('mouseover', () => {
-                div.style.cssText = "background-color: grey;"
+                div.style.cssText = returnBackgroundColor();
               });
             gameContainer.appendChild(div);
         }
@@ -36,6 +53,14 @@ function askForSize() {
 
 function reset() {
     createDivs(sizeRemember);
+}
+
+function normal() {
+    colorChoice = 0;
+}
+
+function rainbow() {
+    colorChoice = 1;
 }
 
 
